db78de2a8256fceab81a9a77308d767cd5fac2c0
mozzammelrobi <mozzammelfb@gmail.com>
2024-04-04 09:31:08 +0600

    left side social side and google github loign and qZoone

--- src/pages/Home/BrekingNews.jsx
+++ src/pages/Home/BrekingNews.jsx
@@ -6,10 +6,10 @@ const BrekingNews = () => {
         <div className="flex">
             <button className="btn btn-secondary">Latest</button>
             <Marquee pauseOnHover={true} speed={100}>
-               <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
-               <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
-               <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
-               <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
+                <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
+                <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
+                <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
+                <Link className="mr-12" to='/'> Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
             </Marquee>
         </div>
     );
--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,42 @@
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 
+import qzoon1 from '../../../assets/qZone1.png'
+import qzoon2 from '../../../assets/qZone2.png'
+import qzoon3 from '../../../assets/qZone3.png'
 
 const RightSideNav = () => {
     return (
         <div>
-            <h4 className="text-4">Righ side nav</h4>
+            <div className="p-4 mb-5">
+                <h2 className="text3xl">Login</h2>
+                <button className="btn btn-outline w-full mb-4">
+                    <FaGoogle></FaGoogle>
+                    Google
+                </button>
+                <button className="btn btn-outline w-full">
+                    <FaGithub></FaGithub>
+                    Github
+                </button>
+            </div>
+
+            <div className="mb-5">
+                <h1 className="text-3xl mb-4">FInd us on</h1>
+               <div className="text-center ">
+               <a className="flex gap-2 border-b w-full pl-4 items-center mb-4 "> <FaFacebook /> Facebook</a>
+                <a className="flex gap-2 border-b w-full pl-4 items-center mb-4"><FaTwitter/> Twitter</a>
+                <a className="flex gap-2 border-b w-full pl-4 items-center"><FaInstagram/> Instragram</a>
+               </div>
+            </div>
+
+            {/* qZoon */}
+
+            <div>
+                <p className="text-3xl">qZoone </p>
+                <img src={qzoon1} alt="" />
+                <img src={qzoon2} alt="" />
+                <img src={qzoon3} alt="" />
+            </div>
+
         </div>
     );
 };
